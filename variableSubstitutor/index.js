0378a8588fd91ef9064e0867869d48b479d63f1b
Vinicius Silva <viniciussilva@hipagesgroup.com.au>
2021-11-22 10:52:25 +1100

    chore: fix

--- variableSubstitutor/index.js
+++ variableSubstitutor/index.js
@@ -16,6 +16,11 @@ var filesInput = core.getInput("files", { required: true });
 var files = filesInput.split(",");
 
 console.log("Substituting variables in the file: ", files);
+
+var variables = process.env;
+
+console.log("Defined variables are: ");
+console.log(variables);
 // var jsonString = JSON.stringify(config);
 
 // var obj = JSON.parse(jsonString);
